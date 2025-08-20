260ce6028c15d4d2d9423f42fb9a761fc3345598
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-08-20 21:17:42 +0530

    created build

--- app/animations/FirstAnimation.tsx
+++ app/animations/FirstAnimation.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import Animated, { useSharedValue, withSpring } from "react-native-reanimated";
+import { StyleSheet, View } from "react-native";
+import { useSharedValue, withSpring } from "react-native-reanimated";
+import Rive from "rive-react-native";
 
 const FirstAnimation = () => {
   const width = useSharedValue(150);
@@ -15,7 +16,7 @@ const FirstAnimation = () => {
 
   return (
     <View className="flex-1 justify-center items-center">
-      <Animated.View
+      {/* <Animated.View
         style={{
           height,
           width,
@@ -27,7 +28,14 @@ const FirstAnimation = () => {
         onPress={handlePress}
       >
         <Text className="text-white font-semibold text-2xl">Expand</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
+      <Rive
+        url="https://public.rive.app/community/runtime-files/2195-4346-avatar-pack-use-case.riv"
+        artboardName="Avatar 1"
+        stateMachineName="avatar"
+        style={{ width: 400, height: 400 }}
+      />
+      ;
     </View>
   );
 };
