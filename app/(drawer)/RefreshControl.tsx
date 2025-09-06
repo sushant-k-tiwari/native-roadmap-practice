eea804d49f6ff1ea0d47258ce03b8d2f4e0bda8a
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-09-06 23:01:17 +0530

    refresh control

---
+++ app/(drawer)/RefreshControl.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const RefreshControl = () => {
+  return (
+    <View>
+      <Text>RefreshControl</Text>
+    </View>
+  );
+};
+
+export default RefreshControl;
