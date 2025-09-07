bea36d6838ecb5c049e290c414047e5ae99b8d39
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-09-07 22:11:57 +0530

    Simple Refresh control with ScrollView

--- app/(drawer)/RefreshControl.tsx
+++ app/(drawer)/RefreshControl.tsx
@@ -1,10 +1,13 @@
+import { Link } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 
 const RefreshControl = () => {
   return (
-    <View>
-      <Text>RefreshControl</Text>
+    <View className="flex-1 items-center justify-center bg-gray-50">
+      <Link href="../components/RefreshControls/Simple" className="px-4 py-2 bg-black text-white rounded-md">
+        <Text>RefreshControl</Text>
+      </Link>
     </View>
   );
 };
--- app/components/RefreshControls/Simple.tsx
+++ app/components/RefreshControls/Simple.tsx
@@ -1,11 +1,29 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+import { RefreshControl } from "react-native-gesture-handler";
 
 const Simple = () => {
+  const [refresh, setRefresh] = useState(false);
+  const onRefresh = () => {
+    setRefresh(true);
+    setTimeout(() => {
+      setRefresh(false);
+    }, 2000);
+  };
+
   return (
-    <View>
-      <Text>Simple</Text>
-    </View>
+    <ScrollView
+      className="flex-1 bg-gray-100"
+      refreshControl={
+        <RefreshControl refreshing={refresh} onRefresh={onRefresh} />
+      }
+    >
+      {[...Array(20).keys()].map((i) => (
+        <View key={i} className="p-4 m-2 bg-white rounded-xl shadow-sm">
+          <Text className="text-lg">Item {i + 1}</Text>
+        </View>
+      ))}
+    </ScrollView>
   );
 };
 
